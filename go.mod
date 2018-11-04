61a1acfc478261f17d33ce74e2985021a962f529
Drew Easley <drew.easley@logikcull.com>
2018-11-04 00:01:39 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/agilesyndrome/go-alexa-test
+
+require (
+	github.com/agilesyndrome/go-alexa-dispatcher v0.0.0-20181104004158-7980e7380c39
+	github.com/agilesyndrome/go-alexa-i18n v0.0.0-20181029001559-d93cb92bc700
+	github.com/arienmalec/alexa-go v0.0.0-20181025212142-975687393e90
+	github.com/stretchr/testify v1.2.2
+)
---
+++ jsontest/jsontest.go
@@ -0,0 +1,97 @@
+package jsontest
+
+import (
+  "encoding/json"
+  "io/ioutil"
+  "fmt"
+  "errors"
+  "github.com/arienmalec/alexa-go"
+  "github.com/agilesyndrome/go-alexa-dispatcher/dispatcher"
+  "github.com/stretchr/testify/assert"
+  "testing"
+  "github.com/agilesyndrome/go-alexa-i18n/alexai18n"
+)
+
+var (
+ TestCases map[string] ExpectedResponse = map[string] ExpectedResponse{}
+)
+
+func Add(tc string, e ExpectedResponse) {
+  TestCases[tc] = e
+}
+
+//Simple Helper function to assume that e is nil
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+
+func checkResponse(testFile string) (alexa.Response, error) {
+   test_data, json_err := ioutil.ReadFile(testFile)
+   check(json_err)
+   testRequest := new(alexa.Request)
+   err := json.Unmarshal(test_data, &testRequest)
+   response, err := dispatcher.Dispatch(*testRequest )
+   return response, err
+
+}
+
+type ExpectedResponse struct {
+  TestFile string
+  Title string
+  Text string
+  Error error
+  Culture string
+  ShouldEndSession bool
+
+  testRequest alexa.Request
+}
+
+
+func Expect(culture string, title string, text string, shouldEndSession bool) (ExpectedResponse) {
+
+  e:= ExpectedResponse {
+    Title : title,
+    Text : text,
+    Culture : culture,
+    ShouldEndSession : shouldEndSession,
+    Error : nil,
+    testRequest : alexai18n.CultureRequest(culture),
+
+  }
+  return e
+}
+
+
+
+
+func ExpectError(culture string, title string, text string, shouldEndSession bool, errorText string) (ExpectedResponse) {
+  e:= Expect(culture, title, text, shouldEndSession)
+  e.Error = errors.New(errorText)
+  return e
+}
+
+func assertResponse(e ExpectedResponse, t *testing.T) {
+   response, err := checkResponse(e.TestFile)
+   expectedAlexaResponse := alexa.NewSimpleResponse(e.Title, e.Text)
+   assert.IsType(t, alexa.Response{}, response)
+   assert.Equal(t, expectedAlexaResponse, response)
+   assert.Equal(t, e.ShouldEndSession, response.Body.ShouldEndSession)
+   assert.Equal(t, e.Error, err)
+}
+
+func Run(tc string, t *testing.T)(ExpectedResponse) {
+  e:= TestCases[ tc ]
+  fmt.Printf("Running %s...\n", tc)
+  e.TestFile = fmt.Sprintf("../tests/data/%s.json", tc)
+  assertResponse(e,t)
+  return e
+}
+
+func RunTests(t *testing.T) {
+  for k, _ := range TestCases {
+    Run(k,t)
+  }
+}
